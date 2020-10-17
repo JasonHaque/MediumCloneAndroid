eb5dcf5a4087f067f5b938bb4b392ac94b62a032
ZarifAhsan <mdzarifahsan@gmail.com>
2020-10-17 12:37:44 +0600

    added menu and initials

--- app/src/main/java/com/example/mediumcloneandroid/MainActivity.kt
+++ app/src/main/java/com/example/mediumcloneandroid/MainActivity.kt
@@ -2,10 +2,44 @@ package com.example.mediumcloneandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var toggle: ActionBarDrawerToggle
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        toggle = ActionBarDrawerToggle(this, drawer_layout, R.string.open, R.string.close)
+        drawer_layout.setDrawerListener(toggle)
+        toggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        navView.setNavigationItemSelectedListener {
+            when(it.itemId) {
+                R.id.item1 -> Toast.makeText(applicationContext, "Item 1 selected",
+                    Toast.LENGTH_SHORT).show()
+                R.id.item2 -> Toast.makeText(applicationContext, "Item 2 selected",
+                    Toast.LENGTH_SHORT).show()
+                R.id.item3 -> Toast.makeText(applicationContext, "Item 3 selected",
+                    Toast.LENGTH_SHORT).show()
+            }
+            true
+        }
     }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (toggle.onOptionsItemSelected(item)) {
+            return true
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
 }
