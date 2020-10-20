ef2791f130485d1be14d033623f006fe29bd10a5
ZarifAhsan <mdzarifahsan@gmail.com>
2020-10-21 03:37:49 +0600

    fragments and viewModels attached

--- app/src/main/java/com/example/mediumcloneandroid/ui/audio/AudioFragment.kt
+++ app/src/main/java/com/example/mediumcloneandroid/ui/audio/AudioFragment.kt
@@ -5,15 +5,28 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.example.mediumcloneandroid.R
+import com.example.mediumcloneandroid.ui.home.HomeViewModel
 
 class AudioFragment : Fragment() {
 
+    private lateinit var audioViewModel: AudioViewModel
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_audio, container, false)
+        audioViewModel =
+            ViewModelProvider(this).get(AudioViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_audio, container, false)
+        val textView: TextView = root.findViewById(R.id.text_audio)
+        audioViewModel.text.observe(viewLifecycleOwner, Observer {
+            textView.text = it
+        })
+        return root
     }
 }
