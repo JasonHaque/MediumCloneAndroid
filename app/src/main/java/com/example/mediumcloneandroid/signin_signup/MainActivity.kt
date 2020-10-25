6fa09a6c3b8efa6544e7ba104b300729ea2fe76c
ZarifAhsan <mdzarifahsan@gmail.com>
2020-10-26 00:12:17 +0600

    Added main login/signup activity layout

---
+++ app/src/main/java/com/example/mediumcloneandroid/signin_signup/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mediumcloneandroid.signin_signup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mediumcloneandroid.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
