252c74dd5c7a85f6d83f0c5cde7a055fc2e40cdb
ZarifAhsan <mdzarifahsan@gmail.com>
2020-10-26 01:33:29 +0600

    Sign up activity layout initialized

--- app/src/main/java/com/example/mediumcloneandroid/signin_signup/MainActivity.kt
+++ app/src/main/java/com/example/mediumcloneandroid/signin_signup/MainActivity.kt
@@ -1,12 +1,29 @@
 package com.example.mediumcloneandroid.signin_signup
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.mediumcloneandroid.MainUi
 import com.example.mediumcloneandroid.R
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        checkAuthStatus()
+        bindListeners()
+    }
+
+    private fun bindListeners() {
+
+    }
+
+    private fun checkAuthStatus() {
+        if (FirebaseAuth.getInstance().currentUser != null) {
+            startActivity(Intent(this, MainUi::class.java))
+        }
     }
 }
