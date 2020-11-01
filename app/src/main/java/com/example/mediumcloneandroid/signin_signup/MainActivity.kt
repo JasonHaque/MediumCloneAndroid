b192326c9e90f734e0866a8c38a2f29e68438ab8
ZarifAhsan <mdzarifahsan@gmail.com>
2020-11-01 14:34:22 +0600

    implemented Facebook Authentication

--- app/src/main/java/com/example/mediumcloneandroid/signin_signup/MainActivity.kt
+++ app/src/main/java/com/example/mediumcloneandroid/signin_signup/MainActivity.kt
@@ -4,8 +4,11 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.Toast
 import com.example.mediumcloneandroid.ui.MainUi
 import com.example.mediumcloneandroid.R
+import com.facebook.*
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
@@ -13,8 +16,12 @@ import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.GoogleAuthProvider
 import kotlinx.android.synthetic.main.activity_main.*
-import com.facebook.FacebookSdk;
 import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginResult
+import com.facebook.login.widget.LoginButton
+import com.google.firebase.auth.FacebookAuthProvider
+import com.google.firebase.auth.FirebaseUser
+import kotlinx.android.synthetic.main.activity_main.view.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -24,6 +31,8 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var mAuth: FirebaseAuth
     private lateinit var googleSignInClient: GoogleSignInClient
+    private lateinit var callbackManager: CallbackManager
+    private lateinit var binding: LoginButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,10 +47,17 @@ class MainActivity : AppCompatActivity() {
 
         googleSignInClient = GoogleSignIn.getClient(this, gso)
 
+        //Facebook
+        FacebookSdk.sdkInitialize(this)
+        binding = findViewById(R.id.sign_up_with_facebook)
+        facebookAuth()
+
         // Firebase Auth instance
         mAuth = FirebaseAuth.getInstance()
 
         bindListeners()
+
+
     }
 
     private fun bindListeners() {
@@ -105,6 +121,56 @@ class MainActivity : AppCompatActivity() {
 
                 // ...
             }
+    }
+
+    private fun facebookAuth() {
+        // Initialize Facebook Login button
+        callbackManager = CallbackManager.Factory.create()
+
+        binding.sign_up_with_facebook.setReadPermissions("email", "public_profile")
+        binding.sign_up_with_facebook.registerCallback(callbackManager, object :
+            FacebookCallback<LoginResult> {
+
+            override fun onSuccess(loginResult: LoginResult) {
+                Log.d("MainActivity", "facebook:onSuccess:$loginResult")
+                handleFacebookAccessToken(loginResult.accessToken)
+            }
+
+            override fun onCancel() {
+            }
+
+            override fun onError(error: FacebookException) {
+            }
+        })
+    }
+
+    private fun handleFacebookAccessToken(token: AccessToken) {
+        val credential = FacebookAuthProvider.getCredential(token.token)
+        mAuth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d("MainActivity", "signInWithCredential:success")
+                    val user = mAuth.currentUser
+                    updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w("MainActivity", "signInWithCredential:failure", task.exception)
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+                    updateUI(null)
+                }
+
+                // ...
+            }
+    }
+
+    private fun updateUI(user: FirebaseUser?) {
+        if (user != null) {
+            startActivity(Intent(this, MainUi::class.java))
+        } else {
+            Toast.makeText(this, "Please sign in to continue", Toast.LENGTH_SHORT).show()
+        }
     }
 
 }
