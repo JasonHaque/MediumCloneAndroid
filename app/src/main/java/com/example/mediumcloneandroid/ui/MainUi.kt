7e571433f80efbd174b582cd6f69107bc7777599
ZarifAhsan <mdzarifahsan@gmail.com>
2020-11-13 19:37:28 +0600

    Set intent for nav header

--- app/src/main/java/com/example/mediumcloneandroid/ui/MainUi.kt
+++ app/src/main/java/com/example/mediumcloneandroid/ui/MainUi.kt
@@ -1,5 +1,6 @@
 package com.example.mediumcloneandroid.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
@@ -8,6 +9,7 @@ import android.view.WindowManager
 import androidx.appcompat.widget.Toolbar
 import androidx.core.content.ContextCompat
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.Navigation
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
@@ -25,6 +27,7 @@ class MainUi : AppCompatActivity() {
 
     private lateinit var mAuth: FirebaseAuth
     private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var navView: NavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,7 +40,7 @@ class MainUi : AppCompatActivity() {
         setSupportActionBar(toolbar)
 
         val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
-        val navView: NavigationView = findViewById(R.id.nav_view)
+        navView = findViewById(R.id.nav_view)
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
         appBarConfiguration = AppBarConfiguration(setOf(
@@ -46,6 +49,8 @@ class MainUi : AppCompatActivity() {
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
 
+
+        bindListeners()
 //        setProfileCredentials()
     }
 
@@ -57,6 +62,13 @@ class MainUi : AppCompatActivity() {
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
+    private fun bindListeners() {
+        val navHeader = navView.getHeaderView(0)
+        navHeader.setOnClickListener {
+            startActivity(Intent(this, UserProfileActivity::class.java))
+        }
     }
 
 //    private fun setProfileCredentials() {
