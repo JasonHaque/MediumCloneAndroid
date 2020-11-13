52d577d8b13df0d70c3d226fbb61d837715e7a24
ZarifAhsan <mdzarifahsan@gmail.com>
2020-11-13 21:43:54 +0600

    Closed drawer when item is clicked

--- app/src/main/java/com/example/mediumcloneandroid/ui/MainUi.kt
+++ app/src/main/java/com/example/mediumcloneandroid/ui/MainUi.kt
@@ -1,27 +1,20 @@
 package com.example.mediumcloneandroid.ui
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
-import android.view.Window
-import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
-import androidx.core.content.ContextCompat
 import androidx.drawerlayout.widget.DrawerLayout
-import androidx.navigation.Navigation
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import com.bumptech.glide.Glide
 import com.example.mediumcloneandroid.R
 import com.google.android.material.navigation.NavigationView
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
-import kotlinx.android.synthetic.main.activity_user_profile.*
 import kotlinx.android.synthetic.main.nav_header_main.*
 
 class MainUi : AppCompatActivity() {
@@ -29,6 +22,7 @@ class MainUi : AppCompatActivity() {
     private lateinit var mAuth: FirebaseAuth
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var navView: NavigationView
+    private lateinit var drawerLayout: DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,13 +34,15 @@ class MainUi : AppCompatActivity() {
         val toolbar: Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
 
-        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        drawerLayout = findViewById(R.id.drawer_layout)
         navView = findViewById(R.id.nav_view)
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
-        appBarConfiguration = AppBarConfiguration(setOf(
-            R.id.nav_home, R.id.nav_audio, R.id.nav_bookmarks, R.id.nav_interests
-        ), drawerLayout)
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.nav_home, R.id.nav_audio, R.id.nav_bookmarks, R.id.nav_interests
+            ), drawerLayout
+        )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
 
@@ -68,6 +64,7 @@ class MainUi : AppCompatActivity() {
     private fun bindListeners() {
         val navHeader = navView.getHeaderView(0)
         navHeader.setOnClickListener {
+            drawerLayout.closeDrawers()
             startActivity(Intent(this, UserProfileActivity::class.java))
         }
     }
