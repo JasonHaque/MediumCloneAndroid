dc272d4ba55d0f1fbae05c1f0cb4a9e198882429
ZarifAhsan <mdzarifahsan@gmail.com>
2020-11-29 21:36:29 +0600

    A lil bug fixed

--- app/src/main/java/com/example/mediumcloneandroid/ui/story/CreateViewModel.kt
+++ app/src/main/java/com/example/mediumcloneandroid/ui/story/CreateViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class CreateViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+        value = "This is Create Story Fragment"
     }
     val text: LiveData<String> = _text
 }
