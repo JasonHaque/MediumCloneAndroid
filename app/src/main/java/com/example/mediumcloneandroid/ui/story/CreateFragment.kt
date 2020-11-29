0a882f23adfc0ee22a5ce1be74af4d6a74666906
ZarifAhsan <mdzarifahsan@gmail.com>
2020-11-29 23:11:05 +0600

    Removed error codes

--- app/src/main/java/com/example/mediumcloneandroid/ui/story/CreateFragment.kt
+++ app/src/main/java/com/example/mediumcloneandroid/ui/story/CreateFragment.kt
@@ -3,11 +3,8 @@ package com.example.mediumcloneandroid.ui.story
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
-import android.view.TextureView
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.mediumcloneandroid.R
 
@@ -15,24 +12,13 @@ class CreateFragment : Fragment() {
 
     private lateinit var createViewModel: CreateViewModel
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         createViewModel = ViewModelProvider(this).get(CreateViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_create, container, false)
-        val textView: TextView = root.findViewById(R.id.text_create)
-        createViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
-        return root
+        return inflater.inflate(R.layout.fragment_create, container, false)
     }
 
 
--- app/src/main/java/com/example/mediumcloneandroid/ui/story/CreateViewModel.kt
+++ app/src/main/java/com/example/mediumcloneandroid/ui/story/CreateViewModel.kt
@@ -1,13 +1,7 @@
 package com.example.mediumcloneandroid.ui.story
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class CreateViewModel : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is Create Story Fragment"
-    }
-    val text: LiveData<String> = _text
 }
