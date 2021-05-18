4c221935e310241f1f4d830ddd055c64b3467c00
ZarifAhsan <mdzarifahsan@gmail.com>
2021-05-18 16:04:28 +0600

    changed declared classes

--- app/src/main/java/com/example/mediumcloneandroid/adapters/StoryItemAdapter.kt
+++ app/src/main/java/com/example/mediumcloneandroid/adapters/StoryItemAdapter.kt
@@ -1,26 +1,40 @@
 package com.example.mediumcloneandroid.adapters
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.mediumcloneandroid.R
 
 class StoryItemAdapter : RecyclerView.Adapter<StoryItemAdapter.ViewHolder>() {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val inflater: LayoutInflater = LayoutInflater.from(parent.context)
+        val view: View = inflater.inflate(R.layout.story_item, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView), View.OnClickListener {
+
+        lateinit var imageView: ImageView
+        lateinit var storyTitle: TextView
+        lateinit var timePosted: TextView
+        lateinit var datePosted: TextView
 
         override fun onClick(v: View?) {
             TODO("Not yet implemented")
         }
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        TODO("Not yet implemented")
-    }
-
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("Not yet implemented")
-    }
-
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
 }
---
+++ app/src/main/java/com/example/mediumcloneandroid/data/StoryItem.kt
@@ -0,0 +1,6 @@
+package com.example.mediumcloneandroid.data
+
+import android.widget.ImageView
+
+data class StoryItem(val imageView: Int, val storyTitle: String,
+                     val postedTime: String, val postedDate: String)
