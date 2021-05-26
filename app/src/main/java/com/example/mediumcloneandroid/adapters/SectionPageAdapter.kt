23719cfcb6bff20f8eb5a8610e3fdb08fde56f0b
ZarifAhsan <mdzarifahsan@gmail.com>
2021-05-26 06:46:52 +0600

    added viewpager and adapters to my story meny

---
+++ app/src/main/java/com/example/mediumcloneandroid/adapters/SectionPageAdapter.kt
@@ -0,0 +1,27 @@
+package com.example.mediumcloneandroid.adapters
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.Lifecycle
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.example.mediumcloneandroid.ui.tab_fragments.DraftFragment
+import com.example.mediumcloneandroid.ui.tab_fragments.PublishedFragment
+
+class SectionPageAdapter(fm: FragmentManager, lifecycle: Lifecycle)
+    : FragmentStateAdapter(fm, lifecycle) {
+
+
+    private var titleList: List<String> = ArrayList()
+
+    override fun getItemCount(): Int {
+        return 2
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        return when(position) {
+            0 -> {PublishedFragment()}
+            1 -> {DraftFragment()}
+            else -> {Fragment()}
+        }
+    }
+}
--- app/src/main/java/com/example/mediumcloneandroid/ui/my_story/MyStoryFragment.kt
+++ app/src/main/java/com/example/mediumcloneandroid/ui/my_story/MyStoryFragment.kt
@@ -8,7 +8,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.viewpager2.widget.ViewPager2
 import com.example.mediumcloneandroid.R
+import com.example.mediumcloneandroid.adapters.SectionPageAdapter
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MyStoryFragment : Fragment() {
 
@@ -22,10 +26,21 @@ class MyStoryFragment : Fragment() {
         myStoryViewModel =
             ViewModelProvider(this).get(MyStoryViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_my_stories, container, false)
-        val textView: TextView = root.findViewById(R.id.text_interests)
-        myStoryViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+
+        val tabLayout = root.findViewById<TabLayout>(R.id.tabLayout)
+        val viewPager = root.findViewById<ViewPager2>(R.id.viewPager)
+
+        val adapter = SectionPageAdapter(requireActivity().supportFragmentManager, lifecycle)
+
+        viewPager.adapter = adapter
+
+        TabLayoutMediator(tabLayout, viewPager) {tab, position ->
+            when(position) {
+                0 -> {tab.text = "Published Fragment"}
+                1 -> {tab.text = "Draft Fragment"}
+            }
+        }.attach()
+
         return root
     }
 }
