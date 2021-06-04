79fc3039f2a61585438d802af97e179416607aa3
ZarifAhsan <mdzarifahsan@gmail.com>
2021-06-04 09:05:26 +0600

    Made full story activity a little better

--- app/src/main/java/com/example/mediumcloneandroid/ui/story/FullStoryActivity.kt
+++ app/src/main/java/com/example/mediumcloneandroid/ui/story/FullStoryActivity.kt
@@ -2,6 +2,7 @@ package com.example.mediumcloneandroid.ui.story
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
 import com.example.mediumcloneandroid.R
 import kotlinx.android.synthetic.main.activity_full_story.*
 
@@ -17,18 +18,23 @@ class FullStoryActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_full_story)
 
-        val toolbar = toolbar_story
-        
         title = intent.getStringExtra("Title").toString()
         date = intent.getStringExtra("Date").toString()
         author = intent.getStringExtra("Author").toString()
         image = intent.getStringExtra("Image").toString()
         story = intent.getStringExtra("Story").toString()
 
-        toolbar.title = title
-        full_story.text = story
 
-        setSupportActionBar(toolbar)
+        if (image == "null") {
+            image_full_story.setImageResource(R.drawable.light_tile)
+        } else {
+            Glide.with(applicationContext).load(image).into(image_full_story)
+        }
+
+        title_full_story.text = title
+        full_story.text = story
+//
+//        setSupportActionBar(toolbar)
 
     }
 
